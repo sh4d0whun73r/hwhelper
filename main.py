37e8f790f21f03d09d0dae202bc4423a7b70f3b9
Alexander <qiang.jiayuan@dhs.sg>
2020-03-31 18:38:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import csv
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  if request.method == 'GET':
+    return render_template('index.html')
+  else:
+    name = request.form['name']
+    email = request.form['email']
+    question = request.form['question']
+    with open('uploads/questions.csv','a') as csvfile:
+      writer = csv.writer(csvfile, delimiter=',')
+      writer.writerow([name,email,question])
+    return render_template('view.html', n=name, q=question, e=email)
+
+def read_csv(filename):
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        return [dict(row) for row in reader]
+
+@app.route('/view')
+def testing():
+    csv_data = read_csv('uploads/questions.csv')
+    headers = csv_data[0].keys()
+    return render_template('view.html', csv_data=csv_data, headers=headers)
+
+app.run(host='0.0.0.0', port=8080)
+
+if __name__ == '__main__':
+  app.run(debug=True)
